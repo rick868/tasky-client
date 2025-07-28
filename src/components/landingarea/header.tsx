e9145b766d805cd48bb7389e5217eb1793afe28a
Your Name <your.email@example.com>
2025-07-28 13:45:06 +0300

    updated the feature-showcase section

--- src/components/landingarea/header.tsx
+++ src/components/landingarea/header.tsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
-    <header style={{ padding: '0.5rem', backgroundColor: '#2f333bff', color: 'white' }}>
-      <h1>Tasky App</h1>
+    <header style={{ padding: '0.3rem', backgroundColor: '#282c34', color: 'white' }}>
+      <h1>
+        <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+          Tasky App
+        </Link>
+      </h1>
     </header>
   );
 };
