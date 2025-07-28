1152f096683139b1721621ecb03add5665b5fc76
Your Name <your.email@example.com>
2025-07-28 13:51:42 +0300

    updated the updateTask section

--- src/pages/updateTask.tsx
+++ src/pages/updateTask.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Box, TextField, Button, Typography } from '@mui/material';
 import { useParams, useNavigate } from 'react-router-dom';
 
