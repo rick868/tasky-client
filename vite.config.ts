7777a07618055c87a82f1e9d4a1a613cca5597c5
Your Name <your.email@example.com>
2025-07-29 16:39:34 +0300

    changed the server api

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
-  },
+    chunkSizeWarningLimit: 4096,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          mui: ['@mui/material', '@mui/icons-material']
+        }
+      }
+    }
+  }
 })
